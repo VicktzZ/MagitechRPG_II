aed25bfb82f160d2343fb2023d71fe6454d31265
Vicktz <71114564+VicktzZ@users.noreply.github.com>
2024-03-21 18:01:48 +0000

    Code Sandbox Fix

--- components/misc/RequireAuth.tsx
+++ components/misc/RequireAuth.tsx
@@ -3,28 +3,34 @@
 import { Backdrop, CircularProgress } from '@mui/material';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import React, { type ReactElement, useEffect } from 'react'
+import React, { type ReactElement, useEffect } from 'react';
 
-export default function RequireAuth({ children }: { children: ReactElement | ReactElement[]}): ReactElement {
-    const { data: session, status } = useSession()
-    const router = useRouter()
+export default function RequireAuth({
+  children
+}: {
+  children: ReactElement | ReactElement[];
+}): ReactElement {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-    useEffect(() => {
-        if (status === 'unauthenticated') {
-            router.push('/')
-        } else if (!localStorage.getItem('user') && session?.user) {
-            localStorage.setItem('user', JSON.stringify(session?.user))
-        }
-        
-    }, [ status, router, session?.user ])
+  // useEffect(() => {
+  //     if (status === 'unauthenticated') {
+  //         router.push('/')
+  //     } else if (!localStorage.getItem('user') && session?.user) {
+  //         localStorage.setItem('user', JSON.stringify(session?.user))
+  //     }
 
-    return (
-        <>
-            {status === 'authenticated' ? children : (
-                <Backdrop open={true}>
-                    <CircularProgress />
-                </Backdrop>
-            )}
-        </>
-    )
+  // }, [ status, router, session?.user ])
+
+  return (
+    <>
+      {status === 'authenticated' ? (
+        children
+      ) : (
+        <Backdrop open={true}>
+          <CircularProgress />
+        </Backdrop>
+      )}
+    </>
+  );
 }
