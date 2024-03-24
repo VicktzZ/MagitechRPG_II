4c9a10a0e0d9358b7e85c6977bde2641aabb572b
Vitor Santos <VictorHugo220BR@gmail.com>
2024-03-24 15:15:45 -0300

    Hotfix

--- components/misc/RequireAuth.tsx
+++ components/misc/RequireAuth.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { NODE_ENV } from '@constants';
 import { Backdrop, CircularProgress } from '@mui/material';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
@@ -11,7 +10,7 @@ export default function RequireAuth({ children }: { children: ReactElement | Rea
     const router = useRouter()
 
     useEffect(() => {
-        if (status === 'unauthenticated' && NODE_ENV !== 'development') {
+        if (status === 'unauthenticated') {
             router.push('/')
         } else if (!localStorage.getItem('user') && session?.user) {
             localStorage.setItem('user', JSON.stringify(session?.user))
@@ -21,7 +20,7 @@ export default function RequireAuth({ children }: { children: ReactElement | Rea
 
     return (
         <>
-            {status === 'authenticated' || NODE_ENV === 'development' ? children : (
+            {status === 'authenticated' ? children : (
                 <Backdrop open={true}>
                     <CircularProgress />
                 </Backdrop>
