19c3f552e6d55c481df790c77dbef36660fc9390
Vitor Santos <VictorHugo220BR@gmail.com>
2024-04-15 19:17:56 -0300

    Hotfix

--- components/ficha/CreateItemModal.tsx
+++ components/ficha/CreateItemModal.tsx
@@ -1,7 +1,8 @@
-import { MenuItem, TextField, Box, Typography, Grid, ListSubheader, useMediaQuery, Button, Chip } from '@mui/material'
+// import { MenuItem, TextField, Box, Typography, Grid, ListSubheader, useMediaQuery, Button, Chip } from '@mui/material'
+import { Box } from '@mui/material'
 import { useEffect, type ReactElement, useState } from 'react'
 import type { Armor, DamageType, Ficha, Item, MergedItems, Weapon } from '@types'
-import { useTheme } from '@mui/material'
+// import { useTheme } from '@mui/material'
 import { type FormikContextType, useFormikContext } from 'formik'
 import { useSnackbar } from 'notistack'
 import { ArmorModal, ItemModal, WeaponModal } from './CreateModals'
@@ -21,8 +22,8 @@ export default function CreateItemModal({
         quantity: 1
     }
 
-    const theme = useTheme()
-    const matches = useMediaQuery(theme.breakpoints.down('md'))
+    // const theme = useTheme()
+    // const matches = useMediaQuery(theme.breakpoints.down('md'))
     const f: FormikContextType<Ficha> = useFormikContext()
 
     const [ item, setItem ] = useState<Partial<MergedItems<'Leve' | 'Pesada'>>>(
@@ -70,71 +71,71 @@ export default function CreateItemModal({
         setItem({})
     }, [ itemType ])
 
-    // function createItem(): void {
-    //     const isDefaultFieldsFilledIn = !!(
-    //         item.name && item.description &&
-    //         item.rarity && item.kind &&
-    //         item.weight && item.quantity
-    //     )
+    function createItem(): void {
+        const isDefaultFieldsFilledIn = !!(
+            item.name && item.description &&
+            item.rarity && item.kind &&
+            item.weight && item.quantity
+        )
 
-    //     console.log(item, isDefaultFieldsFilledIn);
+        console.log(item, isDefaultFieldsFilledIn);
         
-    //     try { 
-    //         if (itemType === 'weapon') {
-    //             setItem(state => ({ ...state, effect: { ...state.effect, effectType } as any }))
+        try { 
+            if (itemType === 'weapon') {
+                setItem(state => ({ ...state, effect: { ...state.effect, effectType } as any }))
 
-    //             const regex = /^(\d+d\d+)(\+\d+d\d+|\+\d+)*$/
+                const regex = /^(\d+d\d+)(\+\d+d\d+|\+\d+)*$/
                 
-    //             const isAllWeaponFieldsFilledIn = !!(
-    //                 isDefaultFieldsFilledIn && item.range && item.categ &&
-    //                 item.hit && item.ammo && item.accessories &&
-    //                 item.bonus && item.effect?.value &&
-    //                 item.effect?.critChance && item.effect?.critValue &&
-    //                 item.effect?.kind && item.effect?.effectType
-    //             )
+                const isAllWeaponFieldsFilledIn = !!(
+                    isDefaultFieldsFilledIn && item.range && item.categ &&
+                    item.hit && item.ammo && item.accessories &&
+                    item.bonus && item.effect?.value &&
+                    item.effect?.critChance && item.effect?.critValue &&
+                    item.effect?.kind && item.effect?.effectType
+                )
 
-    //             if (!regex.test(item.effect?.value ?? '') ?? !regex.test(item.effect?.critValue ?? '')) {
-    //                 enqueueSnackbar('O dano da arma precisa começar com um dado! EX: 2d4', { variant: 'error', autoHideDuration: 3000 })
-    //             } else {
-    //                 if (!isAllWeaponFieldsFilledIn) {
-    //                     item.categ += ` (${weaponCategParam})` as any
+                if (!regex.test(item.effect?.value ?? '') ?? !regex.test(item.effect?.critValue ?? '')) {
+                    enqueueSnackbar('O dano da arma precisa começar com um dado! EX: 2d4', { variant: 'error', autoHideDuration: 3000 })
+                } else {
+                    if (!isAllWeaponFieldsFilledIn) {
+                        item.categ += ` (${weaponCategParam})` as any
     
-    //                     f.values.inventory.weapons = [ ...f.values.inventory.weapons, item as Weapon<'Leve' | 'Pesada'> ]
-    //                     enqueueSnackbar(`${item.name} criado com sucesso!`, { variant: 'success', autoHideDuration: 3000 })
-    //                     setClosed(true)
-    //                     onClose()
-    //                 } else {
-    //                     enqueueSnackbar('Preencha todos os campos!', { variant: 'error', autoHideDuration: 3000 })
-    //                 }
-    //             }
-    //         } else if (itemType === 'armor') {
-    //             // const isAllArmorFieldsFilledIn = !!(
-    //             //     isDefaultFieldsFilledIn && item.categ &&
-    //             //     item.displacementPenalty && item.value
-    //             // )
+                        f.values.inventory.weapons = [ ...f.values.inventory.weapons, item as Weapon<'Leve' | 'Pesada'> ]
+                        enqueueSnackbar(`${item.name} criado com sucesso!`, { variant: 'success', autoHideDuration: 3000 })
+                        setClosed(true)
+                        onClose()
+                    } else {
+                        enqueueSnackbar('Preencha todos os campos!', { variant: 'error', autoHideDuration: 3000 })
+                    }
+                }
+            } else if (itemType === 'armor') {
+                // const isAllArmorFieldsFilledIn = !!(
+                //     isDefaultFieldsFilledIn && item.categ &&
+                //     item.displacementPenalty && item.value
+                // )
                 
-    //             // if (isAllArmorFieldsFilledIn) {
-    //             f.values.inventory.armors = [ ...f.values.inventory.armors, item as Armor ]
-    //             enqueueSnackbar(`${item.name} criado com sucesso!`, { variant: 'success', autoHideDuration: 3000 })
-    //             setClosed(true)
-    //             onClose()
-    //             // }
-    //         } else {
-    //             // if (isDefaultFieldsFilledIn) {
-    //             f.values.inventory.items = [ ...f.values.inventory.items, item as unknown as Item ]
-    //             enqueueSnackbar(`${item.name} criado com sucesso!`, { variant: 'success', autoHideDuration: 3000 })
-    //             setClosed(true)
-    //             onClose()
-    //             // } else {
-    //             // enqueueSnackbar('Preencha todos os campos!', { variant: 'error', autoHideDuration: 3000 })
-    //             // }
-    //         }
+                // if (isAllArmorFieldsFilledIn) {
+                f.values.inventory.armors = [ ...f.values.inventory.armors, item as Armor ]
+                enqueueSnackbar(`${item.name} criado com sucesso!`, { variant: 'success', autoHideDuration: 3000 })
+                setClosed(true)
+                onClose()
+                // }
+            } else {
+                // if (isDefaultFieldsFilledIn) {
+                f.values.inventory.items = [ ...f.values.inventory.items, item as unknown as Item ]
+                enqueueSnackbar(`${item.name} criado com sucesso!`, { variant: 'success', autoHideDuration: 3000 })
+                setClosed(true)
+                onClose()
+                // } else {
+                // enqueueSnackbar('Preencha todos os campos!', { variant: 'error', autoHideDuration: 3000 })
+                // }
+            }
             
-    //         console.log(item)
-    //     } catch (error: any) {
-    //         enqueueSnackbar(`ERRO: ${error.message}`, { variant: 'error', autoHideDuration: 3000 })
-    //     }
-    // }
+            console.log(item)
+        } catch (error: any) {
+            enqueueSnackbar(`ERRO: ${error.message}`, { variant: 'error', autoHideDuration: 3000 })
+        }
+    }
 
     return (
         <Box
@@ -142,6 +143,7 @@ export default function CreateItemModal({
             flexDirection='column'
             width='100%'
             gap={2}
+            {...createItem}
         >
             { 
                 itemType === 'weapon' ? <WeaponModal /> :
