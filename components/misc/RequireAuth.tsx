d6d0bb9e1c1d796a2b499d81ec5740f266363664
VicktzZ <VictorHugo220Br@gmail.com>
2025-01-15 00:27:59 +0000

    fix: session fixed

--- components/misc/RequireAuth.tsx
+++ components/misc/RequireAuth.tsx
@@ -3,20 +3,26 @@
 import { Backdrop, CircularProgress } from '@mui/material';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import React, { type ReactElement, useEffect } from 'react'
+import React, { type ReactElement, useEffect, useState } from 'react'
 
 export default function RequireAuth({ children }: { children: ReactElement | ReactElement[]}): any {
     const { data: session, status } = useSession()
     const router = useRouter()
+    const [ isLoading, setIsLoading ] = useState(false)
 
     useEffect(() => {
-        if (status !== 'loading') 
-            if (status === 'unauthenticated') {
-                router.push('/')
-            } else if (!localStorage.getItem('user') && session?.user) {
+        if (status === 'loading') setIsLoading(true)
+        
+        if (status !== 'loading') {
+            if (!localStorage.getItem('user') && session?.user) {
                 localStorage.setItem('user', JSON.stringify(session?.user))
             }
-    }, [ status, router, session?.user ])
+        }
+            
+        if (status === 'unauthenticated') {
+            router.push('/')
+        }
+    }, [ status, router, isLoading, session?.user ])
 
     return status === 'authenticated' ? children : (
         <Backdrop open={true}>
