672069714006e49924458ec7ef446f2729f5a180
Vitor Santos <victorhugo220br@gmail.com>
2025-03-03 19:39:39 -0300

    fix: removing cors in prod

--- next.config.js
+++ next.config.js
@@ -28,33 +28,33 @@ const nextConfig = {
             }
         ]
     },
-    async headers() {
-        return [
-            {
-                source: '/api/:path*',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: process.env.NODE_ENV === 'production' 
-                            ? 'https://magitechrpg.vercel.app'
-                            : '*'
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET, POST, PUT, DELETE, OPTIONS'
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'Content-Type, Authorization'
-                    },
-                    {
-                        key: 'Access-Control-Allow-Credentials',
-                        value: 'true'
-                    }
-                ]
-            }
-        ]
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             headers: [
+    //                 {
+    //                     key: 'Access-Control-Allow-Origin',
+    //                     value: process.env.NODE_ENV === 'production' 
+    //                         ? 'https://magitechrpg.vercel.app'
+    //                         : '*'
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Methods',
+    //                     value: 'GET, POST, PUT, DELETE, OPTIONS'
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Headers',
+    //                     value: 'Content-Type, Authorization'
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Credentials',
+    //                     value: 'true'
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // },
     webpack(config) {
         config.experiments = {
             ...config.experiments,
